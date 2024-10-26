febebc26e6a38450476033b6bf5167a20a7b71a4
bubaley <bubaley.fu@gmail.com>
2024-10-26 17:14:57 +0200

    init

--- core/utils/base_urls.py
+++ core/utils/base_urls.py
@@ -1,14 +1,10 @@
 from django.conf import settings
-from django.urls import path, include
 from django.conf.urls.static import static
+from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-if hasattr(settings, 'USE_SILK') and settings.USE_SILK:
-    urlpatterns.insert(0, path('silk/', include('silk.urls', namespace='silk')))
--- user/functions/validate_user_password.py
+++ user/functions/validate_user_password.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.password_validation import validate_password
-from rest_framework import serializers
 from django.core import exceptions
+from rest_framework import serializers
 
 
 def validate_user_password(password):
@@ -8,5 +8,5 @@ def validate_user_password(password):
         validate_password(password)
     except exceptions.ValidationError as e:
         serializer_error = serializers.as_serializer_error(e)
-        raise serializers.ValidationError(serializer_error["non_field_errors"])
+        raise serializers.ValidationError(serializer_error['non_field_errors'])
     return password
