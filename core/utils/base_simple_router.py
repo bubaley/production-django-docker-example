d9d48f2554ae5da0223031e6ccb941e1a7a57bbf
bubaley <bubaley.fu@gmail.com>
2024-10-27 23:51:54 +0200

    fix(router): change router name

--- core/utils/base_simple_router.py
+++ core/utils/base_simple_router.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import SimpleRouter
 
 
-class BaseRouter(SimpleRouter):
+class BaseSimpleRouter(SimpleRouter):
     def __init__(self):
         super().__init__()
         self.trailing_slash = '/?'
--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,7 @@
-from core.utils.base_router import BaseRouter
+from core.utils.base_simple_router import BaseSimpleRouter
 
 from .views import UserViewSet
 
-router = BaseRouter()
+router = BaseSimpleRouter()
 router.register(r'users', UserViewSet, basename='users')
 urlpatterns = router.urls
