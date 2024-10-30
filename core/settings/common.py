fa4afab616c84563952b087a4e61f9eb5bc8527f
bubaley <bubaley.fu@gmail.com>
2024-10-30 23:13:01 +0200

    feat(databases): Add TEST database connection

--- core/settings/common.py
+++ core/settings/common.py
@@ -76,6 +76,9 @@ if env.str('SQL_ENGINE', None):
             'PASSWORD': env.str('SQL_PASSWORD'),
             'HOST': env.str('SQL_HOST'),
             'PORT': env.str('SQL_PORT'),
+            'TEST': {
+                'NAME': 'test_' + env.str('SQL_DATABASE'),  # database for tests
+            },
         }
     }
 else:
