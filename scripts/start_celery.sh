e93cc0e1cc72fa691f5988ca3483fd7573d0c5b0
bubaley <bubaley.fu@gmail.com>
2024-10-30 16:06:40 +0200

    feat(Makefile): Migrate scripts to Makefile

--- scripts/start_celery.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -f .env ]; then
-    export $(grep -v '^#' .env | xargs)
-fi
-
-CELERY_CONCURRENCY=${CELERY_CONCURRENCY:-4}
-
-celery -A core worker -B -E -n worker --loglevel=INFO --concurrency=$CELERY_CONCURRENCY
