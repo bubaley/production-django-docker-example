016c72ef41ac1f891fa664d2dfaf46d24fe59790
bubaley <bubaley.fu@gmail.com>
2024-11-05 02:16:28 +0200

    fix(ready): Move ready function

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,7 @@
-from django.http import JsonResponse
-from django.urls import include, path, re_path
+from django.urls import include, path
 
 from core.utils.urls import urlpatterns
 
 urlpatterns += [
     path('api/v1/', include('user.urls')),
-    re_path('api/v1/ready/?$', lambda request: JsonResponse(data={'success': True})),
 ]
--- core/utils/urls.py
+++ core/utils/urls.py
@@ -1,9 +1,11 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.http import JsonResponse
+from django.urls import path, re_path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    re_path('api/v1/ready/?$', lambda request: JsonResponse(data={'success': True})),
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
